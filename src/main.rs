706724c0a979f587ea23e1f7f5c112229f86b06b
yisus82 <djalma8@gmail.com>
2022-09-29 13:49:40 +0200

    Add paddles

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,84 @@
-use tetra::graphics::{self, Color};
+use tetra::graphics::{self, Color, Texture};
+use tetra::input::{self, Key};
+use tetra::math::Vec2;
 use tetra::{Context, ContextBuilder, State};
 
+const WINDOW_WIDTH: f32 = 1920.0;
+const WINDOW_HEIGHT: f32 = 1080.0;
+const PADDLE_SPEED: f32 = 8.0;
+
 fn main() -> tetra::Result {
-    ContextBuilder::new("Pong", 640, 480)
+    ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
         .quit_on_escape(true)
+        .high_dpi(true)
+        .fullscreen(true)
         .build()?
-        .run(|_| Ok(GameState {}))
+        .run(GameState::new)
 }
 
-struct GameState {}
+struct Entity {
+    texture: Texture,
+    position: Vec2<f32>,
+}
+
+impl Entity {
+    fn new(texture: Texture, position: Vec2<f32>) -> Entity {
+        Entity { texture, position }
+    }
+}
+
+struct GameState {
+    player1: Entity,
+    player2: Entity,
+}
+
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+        let player1_texture = Texture::new(ctx, "./img/player1.png")?;
+        let player1_position = Vec2::new(
+            16.0,
+            (WINDOW_HEIGHT - player1_texture.height() as f32) / 2.0,
+        );
+
+        let player2_texture = Texture::new(ctx, "./img/player2.png")?;
+        let player2_position = Vec2::new(
+            WINDOW_WIDTH - player2_texture.width() as f32 - 16.0,
+            (WINDOW_HEIGHT - player2_texture.height() as f32) / 2.0,
+        );
+
+        Ok(GameState {
+            player1: Entity::new(player1_texture, player1_position),
+            player2: Entity::new(player2_texture, player2_position),
+        })
+    }
+}
 
 impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
-        // Feel free to change the color to something of your choice!
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        self.player1.texture.draw(ctx, self.player1.position);
+        self.player2.texture.draw(ctx, self.player2.position);
+
+        Ok(())
+    }
+
+    fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+        if input::is_key_down(ctx, Key::W) {
+            self.player1.position.y -= PADDLE_SPEED;
+        }
+
+        if input::is_key_down(ctx, Key::S) {
+            self.player1.position.y += PADDLE_SPEED;
+        }
+
+        if input::is_key_down(ctx, Key::Up) {
+            self.player2.position.y -= PADDLE_SPEED;
+        }
+
+        if input::is_key_down(ctx, Key::Down) {
+            self.player2.position.y += PADDLE_SPEED;
+        }
 
         Ok(())
     }
