de588ba50396ef4ed5f30eddf140634fa95983d1
yisus82 <djalma8@gmail.com>
2022-09-30 12:46:52 +0200

    Adding a ball

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use tetra::graphics::{self, Color, Texture};
+use tetra::graphics::{self, Color, Rectangle, Texture};
 use tetra::input::{self, Key};
 use tetra::math::Vec2;
+use tetra::window::{self, get_height, get_width};
 use tetra::{Context, ContextBuilder, State};
 
 const WINDOW_WIDTH: f32 = 1920.0;
 const WINDOW_HEIGHT: f32 = 1080.0;
 const PADDLE_SPEED: f32 = 8.0;
+const BALL_SPEED: f32 = 10.0;
+const PADDLE_SPIN: f32 = 4.0;
+const BALL_ACC: f32 = 0.5;
 
 fn main() -> tetra::Result {
     ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
@@ -19,17 +23,51 @@ fn main() -> tetra::Result {
 struct Entity {
     texture: Texture,
     position: Vec2<f32>,
+    velocity: Vec2<f32>,
 }
 
 impl Entity {
     fn new(texture: Texture, position: Vec2<f32>) -> Entity {
-        Entity { texture, position }
+        Entity::with_velocity(texture, position, Vec2::zero())
+    }
+
+    fn with_velocity(texture: Texture, position: Vec2<f32>, velocity: Vec2<f32>) -> Entity {
+        Entity {
+            texture,
+            position,
+            velocity,
+        }
+    }
+
+    fn width(&self) -> f32 {
+        self.texture.width() as f32
+    }
+
+    fn height(&self) -> f32 {
+        self.texture.height() as f32
+    }
+
+    fn bounds(&self) -> Rectangle {
+        Rectangle::new(
+            self.position.x,
+            self.position.y,
+            self.width(),
+            self.height(),
+        )
+    }
+
+    fn centre(&self) -> Vec2<f32> {
+        Vec2::new(
+            self.position.x + (self.width() / 2.0),
+            self.position.y + (self.height() / 2.0),
+        )
     }
 }
 
 struct GameState {
     player1: Entity,
     player2: Entity,
+    ball: Entity,
 }
 
 impl GameState {
@@ -37,18 +75,26 @@ impl GameState {
         let player1_texture = Texture::new(ctx, "./img/player1.png")?;
         let player1_position = Vec2::new(
             16.0,
-            (WINDOW_HEIGHT - player1_texture.height() as f32) / 2.0,
+            (get_height(ctx) as f32 - player1_texture.height() as f32) / 2.0,
         );
 
         let player2_texture = Texture::new(ctx, "./img/player2.png")?;
         let player2_position = Vec2::new(
-            WINDOW_WIDTH - player2_texture.width() as f32 - 16.0,
-            (WINDOW_HEIGHT - player2_texture.height() as f32) / 2.0,
+            get_width(ctx) as f32 - player2_texture.width() as f32 - 16.0,
+            (get_height(ctx) as f32 - player2_texture.height() as f32) / 2.0,
         );
+
+        let ball_texture = Texture::new(ctx, "./img/ball.png")?;
+        let ball_position = Vec2::new(
+            get_width(ctx) as f32 / 2.0 - ball_texture.width() as f32 / 2.0,
+            get_height(ctx) as f32 / 2.0 - ball_texture.height() as f32 / 2.0,
+        );
+        let ball_velocity = Vec2::new(-BALL_SPEED, 0.0);
 
         Ok(GameState {
             player1: Entity::new(player1_texture, player1_position),
             player2: Entity::new(player2_texture, player2_position),
+            ball: Entity::with_velocity(ball_texture, ball_position, ball_velocity),
         })
     }
 }
@@ -59,6 +105,7 @@ impl State for GameState {
 
         self.player1.texture.draw(ctx, self.player1.position);
         self.player2.texture.draw(ctx, self.player2.position);
+        self.ball.texture.draw(ctx, self.ball.position);
 
         Ok(())
     }
@@ -78,6 +125,43 @@ impl State for GameState {
 
         if input::is_key_down(ctx, Key::Down) {
             self.player2.position.y += PADDLE_SPEED;
+        }
+
+        self.ball.position += self.ball.velocity;
+
+        let player1_bounds = self.player1.bounds();
+        let player2_bounds = self.player2.bounds();
+        let ball_bounds = self.ball.bounds();
+
+        let paddle_hit = if ball_bounds.intersects(&player1_bounds) {
+            Some(&self.player1)
+        } else if ball_bounds.intersects(&player2_bounds) {
+            Some(&self.player2)
+        } else {
+            None
+        };
+
+        if let Some(paddle) = paddle_hit {
+            self.ball.velocity.x =
+                -(self.ball.velocity.x + (BALL_ACC * self.ball.velocity.x.signum()));
+
+            let offset = (paddle.centre().y - self.ball.centre().y) / paddle.height();
+
+            self.ball.velocity.y += PADDLE_SPIN * -offset;
+        }
+
+        if self.ball.position.y <= 0.0
+            || self.ball.position.y + self.ball.height() >= get_height(ctx) as f32
+        {
+            self.ball.velocity.y = -self.ball.velocity.y;
+        }
+
+        if self.ball.position.x > WINDOW_WIDTH {
+            window::quit(ctx);
+            println!("Player 1 wins!");
+        } else if self.ball.position.x < 0.0 {
+            window::quit(ctx);
+            println!("Player 2 wins!");
         }
 
         Ok(())
